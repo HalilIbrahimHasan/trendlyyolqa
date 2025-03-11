9fed9a09c70778eed3e2a97f2ed118bf58cb3c10
Halil Ibrahim <46765709+HalilIbrahimHasan@users.noreply.github.com>
2025-03-11 05:39:11 -0400

    Update navbar.tsx

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -127,10 +127,10 @@ const Navbar = () => {
                             </SearchIconWrapper>
                         </SearchWrapper>
                         <Typography component={'div'}>
-                            <IconButton color="black">
+                            <IconButton sx={{ color: 'black' }}>
                                 <FavoriteIcon />
                             </IconButton>
-                            <IconButton color="black">
+                            <IconButton sx={{ color: 'black' }}>
                                 <ShoppingCartIcon />
                             </IconButton>
                         </Typography>
